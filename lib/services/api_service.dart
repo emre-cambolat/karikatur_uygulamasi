028b1395d5fde95742128c78448432c941308779
Emre C <cambolatemre@outlook.com>
2022-04-25 18:41:42 +0300

    ui/build.gradle_fix

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:metin_proje/services/api_constants.dart';
 
-// https://labkod.net/meram/meramAPI/getWeather
 class ApiService {
   Future<String> fetchData({required String url}) async {
     final http.Response response = await http.get(
